5ac8cd937c3bda01221b4a7c83722685ca963971
LAD101work <96466764+LAD101work@users.noreply.github.com>
2022-05-06 08:50:00 +0000

    Add enforce_limits cmd to JsDieselGenerator
    Signed-off-by: LAD101work <96466764+LAD101work@users.noreply.github.com>

--- modules/JsDieselGenerator/index.js
+++ modules/JsDieselGenerator/index.js
@@ -2,6 +2,8 @@
 // Copyright 2020 - 2021 Pionix GmbH and Contributors to EVerest
 const { evlog, boot_module } = require('everestjs');
 
+let _output_power_W = 6000; 
+
 function run_generator(mod) {
 
   // code
@@ -15,7 +17,7 @@ function run_generator(mod) {
         request_parameters: {
           limit_type: "Soft",
           ac_current_A: {
-            max_current_A: 77.0,
+            max_current_A: ( _output_power_W / 230 ),
             min_current_A: 0.0,
             max_phase_count: 3,
             min_phase_count: 1,
@@ -33,6 +35,31 @@ function run_generator(mod) {
 
 }
 
+function setOutputPower(mod, inputData) {
+
+  let power_to_be_set = _output_power_W;
+
+  if (typeof inputData.limits_export != "undefined") {
+    if (typeof inputData.limits_export.request_parameters != "undefined") {
+      if (typeof inputData.limits_export.request_parameters.ac_current_A != "undefined") {
+        if (typeof inputData.limits_export.request_parameters.ac_current_A.current_A != "undefined") {
+          power_to_be_set = inputData.limits_export.request_parameters.ac_current_A.current_A;
+        }
+      }
+    }
+  }
+
+  if (0 <= power_to_be_set){
+    if (power_to_be_set <= 7200){
+      _output_power_W = power_to_be_set;
+    } else {
+      _output_power_W = 7200;
+    }
+  } else {
+    _output_power_W = 0;
+  }
+}
+
 function start_api_loop(mod) {
   const update_interval_milliseconds = mod.config.impl.main.update_interval;
 
@@ -41,6 +68,9 @@ function start_api_loop(mod) {
 
 boot_module(async ({ setup, info, config }) => {
   evlog.info('Booting JsDieselGenerator!');
+
+  setup.provides.main.register.enforce_limits(setOutputPower);
+
 }).then((mod) => {
   // loop
   start_api_loop(mod);
